9bafb711519c976df0ba9a11b4c8cde1b2d24c6d
GymBot <gymbot@example.com>
2026-03-25 20:54:40 -0400

    UI: Increase real-time clock size on main screen

--- lib/screens/setup_screen.dart
+++ lib/screens/setup_screen.dart
@@ -81,7 +81,7 @@ class _SetupScreenState extends State<SetupScreen> {
       body: Column(
         children: [
           Expanded(
-            flex: 2,
+            flex: 3,
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: GridView.count(
@@ -146,10 +146,10 @@ class _SetupScreenState extends State<SetupScreen> {
             ),
           ),
           Expanded(
-            flex: 1,
+            flex: 2,
             child: Center(
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
                 child: FittedBox(
                   fit: BoxFit.contain,
                   child: Text(
@@ -157,6 +157,7 @@ class _SetupScreenState extends State<SetupScreen> {
                     style: const TextStyle(
                       fontWeight: FontWeight.bold,
                       fontFamily: 'monospace',
+                      color: Colors.white,
                     ),
                   ),
                 ),
