bd08d7767f9103e245e8fef76f29a4db2349fde9
GymBot <gymbot@example.com>
2026-04-08 22:11:39 -0400

    Restore 5-second Get Ready countdown and re-attempt start sound synchronization.

--- gym_timer/lib/bloc/timer_bloc.dart
+++ gym_timer/lib/bloc/timer_bloc.dart
@@ -42,7 +42,7 @@ class TimerBloc extends Bloc<TimerEvent, TimerState> {
     // Sync setup clock to Chromecast immediately on start to transition
     _updateCast(0, "Get Ready");
     
-    // 5-second countdown with beeps
+    // 5-second countdown
     for (int i = 5; i > 0; i--) {
       emit(TimerCountdown(i));
       _updateCast(i, "Get Ready");
@@ -55,8 +55,6 @@ class TimerBloc extends Bloc<TimerEvent, TimerState> {
     await _playSound('start.mp3'); // Wait for start sound to finish
     emit(TimerRunInProgress(event.duration));
     _updateCast(event.duration, "Go!");
-
-    // Start the ticker ONLY AFTER the start sound is completely done
     _tickerSubscription?.cancel();
     _tickerSubscription = _ticker
         .tick(ticks: event.duration)
@@ -132,16 +130,10 @@ class TimerBloc extends Bloc<TimerEvent, TimerState> {
 
   Future<void> _playSound(String sound) async {
     try {
-      final player = AudioPlayer(); // Create a new instance for each play
-      final completer = Completer<void>();
-      player.onPlayerComplete.listen((_) {
-        player.dispose(); // Dispose after completion
-        completer.complete();
-      });
-      await player.play(AssetSource('sounds/$sound'));
-      return completer.future;
+      await _audioPlayer.stop();
+      await _audioPlayer.play(AssetSource('sounds/$sound'));
     } catch (e) {
-      return Future.value(); // Complete immediately on error
+      // Ignore
     }
   }
 }
